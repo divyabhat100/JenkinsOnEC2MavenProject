84b946c96a8411fafc4528a3f8eb372058de38af
divyabhat100 <divyabhat100@users.noreply.github.com>
2016-01-21 21:12:50 +0530

    Update TestBankAccount.java

--- src/test/java/TestBankAccount.java
+++ src/test/java/TestBankAccount.java
@@ -11,4 +11,10 @@ public class TestBankAccount {
        double amount = account.debit(5);
        Assert.assertEquals(5.0, amount);
     }
+    @Test
+    public void insufficienttestFunds() {
+       BankAccount account = new BankAccount(10);
+       double amount = account.debit(11);
+       Assert.assertEquals(10.0, amount);
+    }
 }
